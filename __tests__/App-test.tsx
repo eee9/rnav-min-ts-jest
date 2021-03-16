e59699af78d5dddd4898916c71534b0ed31a40cb
eee9 <eee9@i.ua>
2021-03-16 13:53:59 +0200

    Jest tests stage 2.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,10 +1,95 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable dot-notation */
+/* eslint-disable jest/no-disabled-tests */
 import 'react-native';
 import React from 'react';
 import {App} from '../src/App';
 
+const cc = console.log;
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+//it.skip('renders correctly', () => {
+xit('renders correctly', () => {
   renderer.create(<App />);
+});
+
+interface IData {
+  one: number;
+  two?: string;
+  three: number;
+}
+
+xit('object assignment', () => {
+  const _one = 1;
+  const two = 'message 2';
+  const three = 3;
+  const data: IData = {one: _one, three};
+  data['two'] = two;
+  //cc('data => ', data);
+  expect(data).toEqual({one: _one, two, three});
+});
+
+xtest('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+xtest('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+xtest('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  // Це не спрацює через помилку окгруглення!
+  //expect(value).toBe(0.3);
+  // Це спрацює.
+  expect(value).toBeCloseTo(0.3);
+});
+
+const compileAndroidCode = () => {
+  throw new Error('you are using the wrong JDK');
+};
+
+xtest('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use the exact error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
+
+interface IFetch {
+  (message: string): void;
+}
+const fetchData = async (): Promise<string> => {
+  return new Promise((res, rej) => {
+    res('hi');
+  });
+  //return 'Hi!';
+};
+
+test('the data is peanut butter', async () => {
+  const data = await fetchData();
+  //cc('data => "' + data + '"');
+  expect(data).toBe('hi');
+});
+
+xtest('the fetch fails with an error', async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
 });
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,6 +1,24 @@
 //const sum = require('./sum');
 import {sum} from './sum';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 2 + 1 to equal 3', () => {
+  expect(sum(2, 1)).toBe(3);
+});
+
+test('adds 2 + 5 to equal 7', () => {
+  expect(sum(2, 5)).toBe(7);
+});
+
+test('default sum() equals 0', () => {
+  expect(sum()).toBe(0);
+});
+
+test('sum(value) equals value', () => {
+  const value = 77;
+  expect(sum(value)).toBe(value);
+});
+
+test('value - value = 0', () => {
+  const value = 54;
+  expect(sum(value, -1 * value)).toBe(0);
 });
